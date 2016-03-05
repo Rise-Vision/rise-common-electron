baf34dcc29d2a2652a433130dd05f36873ed2fa2
Tyler Johnson <mail@tyler-johnson.ca>
2016-03-04 23:21:03 -0500

    Pass original-fs to rimraf
    This way it doesn't use electron's built-in fs so asar files are removed
    like any other.

--- platform.js
+++ platform.js
@@ -345,7 +345,7 @@ module.exports = {
     });
   },
   callRimraf(path, cb) {
-    rimraf(path, cb);
+    rimraf(path, require("original-fs"), cb);
   },
   callMkdirp(path, cb) {
     mkdirp(path, cb);
