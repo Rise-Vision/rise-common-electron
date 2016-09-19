c4bc17306adce0ea7ec40acfc0d31acbab24e5c0
rodrigopavezi <rodrigopavezi@gmail.com>
2016-09-19 18:08:58 -0300

    Added support for BQ template suffix

--- test/unit/bq-client.js
+++ test/unit/bq-client.js
@@ -38,6 +38,18 @@ describe("bigquery client", function() {
     });
   });
 
+  it("makes the post call with template sufix", function() {
+    return bqClient.insert(testTableName, { event: "testEvent" }, "20160101")
+      .then(()=>{
+      assert.ok(/datasets\/Installer_Events/.test(network.httpFetch.lastCall.args[0]));
+    assert.ok(/tables\/events[0-9]{8}/.test(network.httpFetch.lastCall.args[0]));
+    assert.ok(network.httpFetch.lastCall.args[1].headers.Authorization === "Bearer test-token");
+    assert.ok(JSON.parse(network.httpFetch.lastCall.args[1].body).rows[0].json.event === "testEvent");
+    assert.ok(JSON.parse(network.httpFetch.lastCall.args[1].body).templateSuffix === "20160101");
+
+    });
+  });
+
   it("makes the post call without details", function() {
     return bqClient.insert(testTableName, { event: "testEvent" })
     .then(()=>{
@@ -63,7 +75,7 @@ describe("bigquery client", function() {
 
     return bqClient.insert(testTableName, { event: "testEvent" })
     .then(()=>{
-      return bqClient.insert(testTableName, { event: "testEvent" }, hourAhead);
+      return bqClient.insert(testTableName, { event: "testEvent" }, null, hourAhead);
     }).then(()=>{
       assert.equal(network.httpFetch.callCount, 4);
     });
